b9ed7993b2801eab490369098e20986a364aacb3
aday913 <45890246+aday913@users.noreply.github.com>
2018-12-15 16:21:16 -0700

    Removed package files
    Removed package files that were leading to errors in the building of the application

--- Assets/OkayButtonLinearCode.cs
+++ Assets/OkayButtonLinearCode.cs
@@ -8,54 +8,36 @@ public class OkayButtonLinearCode : MonoBehaviour {
     public void NextSceneOnClick(string input)
     {
         int Case = GameObject.FindGameObjectWithTag("Manager").GetComponent<Settings>().QuestionType;
-        Debug.Log("CASE: " + Case);
-        Debug.Log("TESTING: " + input);
-
-        int comparison = 1;
-
+       
         if (Case != 1)
         {
+            Debug.Log("Should be trying to load next scene");
             SceneManager.LoadScene(TargetScene);
         }
         else
         {
             string FHIR_Information = input;
-            Debug.Log("From OB script: " + FHIR_Information);
 
             var Fhir_Client = new FhirClient("http://stu3.test.pyrohealth.net/fhir");
             Fhir_Client.Timeout = (60 * 1000);
 
-            Debug.Log("patient=Patient/" + FHIR_Information);
-
             Hl7.Fhir.Model.Bundle ReturnedSearchBundle = Fhir_Client.Search<Hl7.Fhir.Model.Observation>(new string[] { "patient=Patient/" + FHIR_Information});
-
-            Debug.Log("GOT A BUNDLE");
 
             string id = "";
 
             foreach (var Entry in ReturnedSearchBundle.Entry)
             {
-                Debug.Log(Entry.Resource.Id);
                 id = Entry.Resource.Id;
             }
 
-            Debug.Log(id);
-
             var obs = Fhir_Client.Read<Observation>("Observation/" + id);
-
-            Debug.Log("WE HAVE READ THE OBSERVATION");
-            Debug.Log(obs.Interpretation.Coding[0].Code);
-            Debug.Log(obs.Code.Coding[0].Code);
-            Debug.Log(obs.Code.Coding[0].Code.GetType());
 
             if (obs.Code.Coding[0].Code == "23537-4")
             {
-                Debug.Log("WE HAVE MOVED INTO THE LOINC IF");
 
                 if (obs.Interpretation.Coding[0].Code == "H")
                 {
                     SceneManager.LoadScene(13);
-                    Debug.Log("Should have loaded in scene 13");
                 }
                 else
                 {
